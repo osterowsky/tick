b4be79cd40e49600a6ad1d60ad7731348c130671
Patryk Ostern <patryk.ostern@justwatch.com>
2023-05-31 10:49:47 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+// Serve static files from the "src directory
+app.use(express.static('src'));
+
+
+// Serve the landing page
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/src/static/index.html');
+});
+
+// Serve the landing page
+app.get('/demo', (req, res) => {
+  res.sendFile(__dirname + '/src/static/demo.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+document.querySelector('.demo-button').addEventListener('click', function() {
+    
+    window.location.href = "../static/demo.html";
+});
+
+function checkoutPage() {
+    console.log("Hello");
+}
